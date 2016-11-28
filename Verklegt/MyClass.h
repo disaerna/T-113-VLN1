057c6f7a710e534994c0c854e74a95e1cbf12946
korsbacka <karla16@ru.is>
2016-11-28 11:52:13 +0000

    klasi

--- Verklegt/MyClass.h
+++ Verklegt/MyClass.h
@@ -9,6 +9,10 @@ class MyClass
         ~MyClass();
 
     private:
+        string name; // nafn
+        string sex; // kyn
+        int dob; // date of birth
+        int dod; // date of death
 };
 
 #endif // MYCLASS_H
