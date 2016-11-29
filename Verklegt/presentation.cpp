e071202c4d4c31de1501b473a4544595f13a9dcf
korsbacka <karla16@ru.is>
2016-11-29 11:49:07 +0000

    ui

--- Verklegt/presentation.cpp
+++ Verklegt/presentation.cpp
@@ -11,9 +11,13 @@ Presentation::Presentation() {
 
 void Presentation::program() {
     cout << "Please enter one of the following commands: " << endl;
-    cout << "1. Add a new person to the database" << endl;
-    cout << "2. Delete a person from the database" << endl;
-    cout << "3. View the database" << endl;
-    cout << "4. Search the database" << endl;
-    cout << "5. Quit the program" << endl;
+    cout << "add - Add a new person to the database" << endl;
+    cout << "delete - Delete a person from the database" << endl;
+    cout << "view - View the database" << endl;
+    cout << "search - Search the database" << endl;
+    cout << "quit - Quit the program" << endl;
+
+    string input = " ";
+    cin >> string;
+
 }
