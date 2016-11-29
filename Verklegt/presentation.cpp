625e0cd0f198a0f18d086f95b75d49e1d8305e07
korsbacka <karla16@ru.is>
2016-11-29 12:01:32 +0000

    ui

--- Verklegt/presentation.cpp
+++ Verklegt/presentation.cpp
@@ -22,22 +22,52 @@ void Presentation::program() {
 
 
     if(input == "add" || input == "Add" || input == "ADD") {
-
+        // add person into database
     }
     else if(input == "delete" || input == "Delete" || input == "DELETE") {
-
+        // delete person from the database
     }
     else if(input == "view" || input == "View" || input == "VIEW") {
+        cout << "How would you like view the database? " << endl;
+        cout << "1. Name: A-Z" << endl;
+        cout << "2. Name: Z-A" << endl;
+        cout << "3. Gender: F-M" << endl;
+        cout << "4. Gender: M-F" << endl;
+        cout << "5. Birth Year: Desc." << endl;
+        cout << "6. Birth Year: Asc." << endl;
+        int viewInput = 0;
+        cin >> viewInput;
 
+        if(input == "1") {
+            // sort name descending
+        }
+        else if(input == "2") {
+            // sort name ascending
+        }
+        else if(input == "3") {
+            // sort gender descending
+        }
+        else if(input == "4") {
+            // sort gender ascending
+        }
+        else if(input == "5") {
+            // sort birth year desc.
+        }
+        else if(input == "6") {
+            // sort birth year asc.
+        }
+        else {
+            cout << "Please enter a valid number!" << endl; // villutékk
+        }
     }
     else if(input == "search" || input == "Search" || input == "SEARCH") {
-
+        // search the database
     }
     else if(input == "quit" || input == "Quit" || input == "QUIT") {
-
+        // quit the program
     }
     else {
+            // villutékk
         cout << "Please enter a valid command!" << endl;
     }
-
 }
