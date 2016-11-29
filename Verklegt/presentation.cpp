61fd39636be3e964249231b965fda318e19fe187
korsbacka <karla16@ru.is>
2016-11-29 11:47:39 +0000

    ui

--- Verklegt/presentation.cpp
+++ Verklegt/presentation.cpp
@@ -4,3 +4,16 @@ Presentation::Presentation()
 {
 
 }
+
+Presentation::Presentation() {
+
+}
+
+void Presentation::program() {
+    cout << "Please enter one of the following commands: " << endl;
+    cout << "1. Add a new person to the database" << endl;
+    cout << "2. Delete a person from the database" << endl;
+    cout << "3. View the database" << endl;
+    cout << "4. Search the database" << endl;
+    cout << "5. Quit the program" << endl;
+}
