2f4f21817af794c32710e57553b25fa32bb4e231
korsbacka <karla16@ru.is>
2016-11-30 14:07:05 +0000

    search functions

--- Verklegt/domain.cpp
+++ Verklegt/domain.cpp
@@ -175,7 +175,7 @@ vector<Persons> Domain:: SortPersons(vector<Persons> getPerson, int viewInput)
     void Domain::searchName(vector<Persons>& vec, vector<int>& results, int input) {
         for(size_t i = 0; i < vec.size(); i++) {
             if(vec[i].getName() == input) {
-                results.push_back(i); // if index is found, it is pushed back to results vector
+                results.push_back(i);
             }
         }
     }
@@ -183,7 +183,7 @@ vector<Persons> Domain:: SortPersons(vector<Persons> getPerson, int viewInput)
     void Domain::searchGender(vector<Persons>& vec, vector<int>& results, int input) {
         for(size_t i = 0; i < vec.size(); i++) {
             if(vec[i].getGender() == input) {
-                results.push_back(i); // if index is found, it is pushed back to results vector
+                results.push_back(i);
             }
         }
     }
@@ -192,7 +192,7 @@ vector<Persons> Domain:: SortPersons(vector<Persons> getPerson, int viewInput)
     void Domain::searchBirthYear(vector<Persons>& vec, vector<int>& results, int input) {
         for(size_t i = 0; i < vec.size(); i++) {
             if(vec[i].getYearOfBirth() == input) {
-                results.push_back(i); // if index is found, it is pushed back to results vector
+                results.push_back(i);
             }
         }
     }
@@ -200,7 +200,7 @@ vector<Persons> Domain:: SortPersons(vector<Persons> getPerson, int viewInput)
     void Domain::searchDeathYear(vector<Persons>& vec, vector<int>& results, int input) {
         for(size_t i = 0; i < vec.size(); i++) {
             if(vec[i].getYearOfDeath() == input) {
-                results.push_back(i); // if index is found, it is pushed back to results vector
+                results.push_back(i);
             }
         }
     }
@@ -218,44 +218,22 @@ vector<Persons> Domain:: SortPersons(vector<Persons> getPerson, int viewInput)
 
 
 // á eftir að cutta niður og setja í önnur föll
-void searchDB(vector<Persons> vec) {
+void searchDB() {
     // asking user to choose how he will search
-    int userChoice = 0;
-    cout << "Please enter one of the following commands: " << endl;
-    cout << "1. Search by name" << endl;
-    cout << "2. Search by gender" << endl;
-    cout << "3. Search by year of birth" << endl;
-    cout << "4. Search by year of death" << endl;
-    cout << "5. Return to main menu" << endl;
-    cin >> userChoice;
+    Presentation::displaySearch();
 
     // if user chooses 1/Search by name
     if(userChoice == 1) {
-        string input = " ";
-        cout << "Enter a name to search: ";
-        cin >> input;
+        Presentation::displaySearchName;
 
-        vector<int> results; // vector to keep found indexes
+        Domain::searchName;
 
-        // for loop to find indexes matching input
-        for(size_t i = 0; i < vec.size(); i++) {
-            if(vec[i].getName() == input) {
-                results.push_back(i); // if index is found, it is pushed back to results vector
-            }
-        }
-
-        int sizeOfResults = results.size(); // size of results vector
+        sizeOfResults = results.size();
 
         // if sizeOfResults = 0, then nothing is found
         // for loop to check output
         if(sizeOfResults != 0) {
-            for(int i = 0; i < sizeOfResults; i++) {
-                cout << vec[i].getName() << " ";
-                cout << vec[i].getGender() << " ";
-                cout << vec[i].getYearOfBirth() << " ";
-                cout << vec[i].getYearOfDeath() << endl;
-            }
-            cout << endl;
+            Presentation::displaySearchResults;
         }
         else {
             cout << "Error" << endl; // error message
@@ -265,123 +243,55 @@ void searchDB(vector<Persons> vec) {
 
     // if user chooses 2/Search by gender
     else if(userChoice == 2) {
-        int input = 0;
-        cout << "Choose one of the following to search: " << endl;
-        cout << "1. Find all males" << endl;
-        cout << "2. Find all females" << endl;
-        cin >> input;
+        Presentation::displaySearchGender;
 
-        vector<int> results; // vector to keep found indexes
+        Domain::searchGender;
 
-        if(input == 1) {
-            for(size_t i = 0; i < vec.size(); i++) {
-                if(vec[i].getGender() == "Male" || vec[i].getGender() == "male") {
-                    results.push_back(i);
-                }
-            }
+        sizeOfResults = results.size();
 
-            int sizeOfResults = results.size();
-
-            if(sizeOfResults != 0) {
-                for(int i = 0; i < sizeOfResults; i++) {
-                    cout << vec[i].getName() << " ";
-                    cout << vec[i].getGender() << " ";
-                    cout << vec[i].getYearOfBirth() << " ";
-                    cout << vec[i].getYearOfDeath() << endl;
-                }
-                cout << endl;
-            }
-            else {
-                cout << "Error" << endl; // error message
-            }
-        }
-        else if(input == 2) {
-            for(size_t i = 0; i < vec.size(); i++) {
-                if(vec[i].getGender() == "Female" || vec[i].getGender() == "female") {
-                    results.push_back(i);
-                }
-            }
-
-            int sizeOfResults = results.size();
-
-            if(sizeOfResults != 0) {
-                for(int i = 0; i < sizeOfResults; i++) {
-                    cout << vec[i].getName() << " ";
-                    cout << vec[i].getGender() << " ";
-                    cout << vec[i].getYearOfBirth() << " ";
-                    cout << vec[i].getYearOfDeath() << endl;
-                }
-                cout << endl;
-            }
-            else {
-                cout << "Error" << endl; // error message
-            }
+        if(sizeOfResults != 0) {
+            Presentation::displaySearchResults;
         }
         else {
-            // error// choose either 1 or 2
+            cout << "Error" << endl; // error message
         }
+
     }
 
 
 
     // if user chooses 3/search by birth year
     if(userChoice == 3) {
-        int input = 0;
-        cout << "Enter a year to search: ";
-        cin >> input;
+        Presentation::displaySearchBirthYear;
 
-        vector<int> results; // vector to keep found indexes
+        Domain::searchBirthYear;
 
-        for(size_t i = 0; i < vec.size(); i++) {
-            if(vec[i].getYearOfBirth() == input) {
-                results.push_back(i);
-            }
-        }
-
-        int sizeOfResults = results.size();
+        sizeOfResults = results.size();
 
         if(sizeOfResults != 0) {
-            for(int i = 0; i < sizeOfResults; i++) {
-                cout << vec[i].getName() << " ";
-                cout << vec[i].getGender() << " ";
-                cout << vec[i].getYearOfBirth() << " ";
-                cout << vec[i].getYearOfDeath() << endl;
-            }
-            cout << endl;
+            Presentation::displaySearchResults;
         }
         else {
             cout << "Error" << endl; // error message
         }
+
     }
 
     // if user chooses 4/search by death year
     if(userChoice == 4) {
-        int input = 0;
-        cout << "Enter a year to search (input 0 for alive persons): ";
-        cin >> input;
+        Presentation::displaySearchDeathYear;
 
-        vector<int> results; // vector to keep found indexes
+        Domain::searchDeathYear;
 
-        for(size_t i = 0; i < vec.size(); i++) {
-            if(vec[i].getYearOfDeath() == input) {
-
-            }
-        }
-
-        int sizeOfResults = results.size();
+        sizeOfResults = results.size();
 
         if(sizeOfResults != 0) {
-            for(int i = 0; i < sizeOfResults; i++) {
-                cout << vec[i].getName() << " ";
-                cout << vec[i].getGender() << " ";
-                cout << vec[i].getYearOfBirth() << " ";
-                cout << vec[i].getYearOfDeath() << endl;
-            }
-            cout << endl;
+            Presentation::displaySearchResults;
         }
         else {
             cout << "Error" << endl; // error message
         }
+
     }
 
     // if user chooses 5/return to main menu
