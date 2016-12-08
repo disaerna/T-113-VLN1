e7fde116b70011428e81907887bd580cfc00cde2
HaukurJ <haukurjul@gmail.com>
2016-12-08 12:28:53 +0000

    Revert "Formatting"
    This reverts commit f7076c4a258a983d9221f36e62ab98cf23b816df.

--- Verklegt/dbmanager.cpp
+++ Verklegt/dbmanager.cpp
@@ -146,11 +146,7 @@ vector<string> DbManager::readComputersAndPersons(int input)
     {
         vector<string> printComputersAndAllPersons;
 
-        query.prepare("SELECT c.Name AS cname, s.Name AS sname"
-                      "FROM ScientistsAndComputers sc"
-                      "INNER JOIN Scientists s ON s.ID = sc.ScientistID"
-                      "INNER JOIN Computers c ON c.ID = sc.ComputerID"
-                      "ORDER BY c.Name ASC");
+        query.prepare("SELECT c.Name AS cname, s.Name AS sname FROM ScientistsAndComputers sc INNER JOIN Scientists s ON s.ID = sc.ScientistID INNER JOIN Computers c ON c.ID = sc.ComputerID ORDER BY c.Name ASC");
         query.exec();
         while(query.next())
         {
@@ -167,11 +163,7 @@ vector<string> DbManager::readComputersAndPersons(int input)
     {
         vector<string> printPersonsAndAllComputers;
 \
-        query.prepare("SELECT s.Name AS sname, c.Name AS cname"
-                      "FROM ScientistsAndComputers sc"
-                      "INNER JOIN Scientists s ON s.ID = sc.ScientistID"
-                      "INNER JOIN Computers c ON c.ID = sc.ComputerID"
-                      "ORDER BY s.Name ASC");
+        query.prepare("SELECT s.Name AS sname, c.Name AS cname FROM ScientistsAndComputers sc INNER JOIN Scientists s ON s.ID = sc.ScientistID INNER JOIN Computers c ON c.ID = sc.ComputerID ORDER BY s.Name ASC");
         query.exec();
 
         while(query.next())
