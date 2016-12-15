731f1a03a2d014ed0c06c6babe93b21c23272c17
HaukurJ <haukurjul@gmail.com>
2016-12-15 16:01:08 +0000

    z

--- Verklegt/dbmanager.h
+++ Verklegt/dbmanager.h
@@ -51,7 +51,7 @@ public:
     vector<int> getIDs(string table);
 
     bool updateScientist(int ID, string name, string gender, string yob, string yod);
-    bool updateComputer(int ID, string updateChoice, string newRecord);
+    bool updateComputer(int ID,string name, string yearbuilt, string type, bool built);
 
 
 
